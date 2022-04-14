d715a00d296c02354180fb909f0ed01d4548a15e
Kishan <51819559+Kishan19961998@users.noreply.github.com>
2022-04-13 23:46:47 -0700

    Add files via upload

---
+++ build/precache-manifest.f0cce6d15b019a2c4033e2456b35b2d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "21bcb1b73e8e3bba0670",
+    "url": "/static/css/main.be869517.chunk.css"
+  },
+  {
+    "revision": "21bcb1b73e8e3bba0670",
+    "url": "/static/js/main.21bcb1b7.chunk.js"
+  },
+  {
+    "revision": "7be62adf1416100692c9",
+    "url": "/static/css/1.abbb249f.chunk.css"
+  },
+  {
+    "revision": "7be62adf1416100692c9",
+    "url": "/static/js/1.7be62adf.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "07213bc08ece70514f518ecb1882fcc5",
+    "url": "/index.html"
+  }
+];
